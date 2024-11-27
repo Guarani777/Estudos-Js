2015eb105a02b10e8c2dc57be292dc6b44d2d2e2
Fabio Guarani <guaraniads7@gmail.com>
2024-11-27 14:38:34 -0300

    Update Codinn.js

--- Codinn.js
+++ Codinn.js
@@ -42,3 +42,52 @@ Como funciona:
 
 
 Isso é apenas um exemplo básico. Você pode expandir o sistema para incluir mais sintomas ou critérios de urgência!
+
+
+
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+                                                             *27/11/2024*
+
+
+//Começamos declarando o array
+const livros = ["Assim que acaba ", "A hipótese do amor ", "A seleção ", "Trono de Vidro ", "It a coisa ", "Manual de assassinato para boas garotas "];
+
+//criamos o prompt de abertura que vai receber os dados de escolha.
+
+const atendimento = prompt("Bem vindo(a) à Livraria pensar. Digite 1, se é funcionário. Digite 2, se é cliente.");
+
+//Criamos a função funcionário e cliente, que vai executar as ações determinadas para cada grupo.
+ 
+
+function cliente (atendimento){
+var inserir = prompt( "alugar livro minha vida")
+}
+function cliente (atendimeto){
+var inserir = prompt("minha vida")
+var devolução =parseInt(prompt ("minha vida"));
+}
+
+
+
+
+function funcionario(atendimento){
+ var inserir = prompt("Insira um novo livro: ");
+ var posicao = parseInt(prompt ("você deseja adicionar esse livro em qual posição"));
+ const atualizandoLivros = livros.splice(posicao, 0, inserir); 
+  return `O livro foi adicionado com sucesso. Atualmente estamos com os livros: ${livros}`
+}
+
+//Crie aqui a função de cliente, seguindo a mesma sintaxe de contrução de função
+
+
+
+
+//Aqui criamos a condição que vai decidir
+if(atendimento === "1"){
+  console.log(funcionario(atendimento))
+} else if(atendimento === "2"){
+  console.log(cliente(atendimento))
+}
